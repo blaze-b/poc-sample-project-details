6fcaaae608204f4425fd54827ad840ccc8351204
Brian Blaze <brianblaze14@outlook.com>
2023-07-18 14:37:15 +0100

    changes in the format

--- src/main/java/com/mclebtec/controller/EmployeeController.java
+++ src/main/java/com/mclebtec/controller/EmployeeController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "v1/employee")
 @Validated
-
 public class EmployeeController {
 
     private final EmployeeService employeeService;
